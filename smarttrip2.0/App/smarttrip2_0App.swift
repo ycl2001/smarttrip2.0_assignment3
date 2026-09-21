07e46708c5211aee58830e8c5b970d6cb34bd3a0
Yen-Chun Liu <yen-chun.liu@student.uts.edu.au>
2026-09-22 08:43:16 +1000

    feat: configure Core Data persistence controller

--- smarttrip2.0/App/smarttrip2_0App.swift
+++ smarttrip2.0/App/smarttrip2_0App.swift
@@ -5,13 +5,17 @@
 //  Created by Yen-Chun Liu on 20/9/2026.
 //
 
+import CoreData
 import SwiftUI
 
 @main
 struct smarttrip2_0App: App {
+    private let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
